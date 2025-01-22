778f4b2e53ff570231a9d39abf460e9e6075d793
Giorgio Foti <giofoti@i44.red.uits.iu.edu>
2025-01-22 06:54:13 -0500

    changed slurm script for quartz

--- runAll.sh
+++ runAll.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 infile=2ch_kmat-nom_rhoN-nom
-polefile=Poles/poletest
-pWave=P
-remin=0
-remax=1
-repts=100
-immin=0
-immax=1
-impts=100
-polecutoff=1
-sheet=true
 nJobs=1
 numfits=15
+fitsfolder=/N/slate/giofoti/GWA-PWA/Fits/$(date +%F)/$infile
 runtime=6:00:00
-
-mkdir -p Fits Plots Poles
+memory=1G
+ncpus=4
+mkdir -p $fitsfolder
 make clean && make
 
 for((i=1; i<=nJobs; i++))
 do
-sbatch --time=$runtime submit.script $infile $polefile $pWave $remin $remax $repts $immin $immax $impts $polecutoff $sheet "$i" $numfits
+sbatch --time=$runtime --mem=$memory --cpus-per-task=$ncpus -e /N/slate/giofoti/GWA-PWA/Fits/$(date +%F)/$infile-err.$i -o /N/slate/giofoti/GWA-PWA/Fits/$(date +%F)/$infile-out.$i submit.script "$infile" $i $numfits $fitsfolder $ncpus
 done
